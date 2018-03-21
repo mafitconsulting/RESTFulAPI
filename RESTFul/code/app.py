ca090086a14816b379617d53d4e98adf1d55acf7
Mark Fieldhouse <Mark.Fieldhouse@mafitconsulting.co.uk>
2018-03-21 20:08:53 +0000

    Commit rest code

--- RESTFul/code/app.py
+++ RESTFul/code/app.py
@@ -1,14 +1,13 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
-
 
 app = Flask(__name__)
 app.secret_key = 'markf'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) # /auth
+jwt = JWT(app, authenticate, identity)  # /auth
 items = []
 
 
@@ -19,18 +18,42 @@ class Item(Resource):
         #   if item['name'] == name:
         #      return item
 
-        item = next(filter(lambda x: x['name'] == name, items), None)  # next match first item found by filter function \
-                                                                     # None is default value if none found after
+        item = next(filter(lambda x: x['name'] == name, items),
+                    None)  # next match first item found by filter function \
+        # None is default value if none found after
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': "An item with name '{}' already exists".format(name)}, 400 # bad request
+            return {'message': "An item with name '{}' already exists".format(name)}, 400  # bad request
 
         data = request.get_json()  # get json payload
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items  # lets use the global items variable (this list) an not local items
+        items = list(filter(lambda x: x['name'] != name,
+                            items))  # We are going to overwrite existing list with new list excluding this. found element
+        return {'message': "item deleted"}
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+                            type=float,
+                            required=True,
+                            help='This field cannot be left blank'
+                            )
+        data = parser.parse_args()  # get json payload
+
+        item = next(filter(lambda x: x['name'] == name, items), None)  # check if item exists?
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
 class ItemList(Resource):
--- RESTFul/code/security.py
+++ RESTFul/code/security.py
@@ -1,10 +1,8 @@
-from werkzeug.security import safe_str_cmp # flask safe way of comparing strings
+from werkzeug.security import safe_str_cmp  # flask safe way of comparing strings
 from user import User
 
-
 users = [
-    User(1, 'bob', 'Tc0nnection'),
-    User(2, 'Mark', 'Tc0nnection')
+    User(1, 'bob', 'abcd')
 ]
 
 # Set comprehension
@@ -12,10 +10,11 @@ username_mapping = {u.username: u for u in users}
 userid_mapping = {u.id: u for u in users}
 
 
-def authenticate(username,password):
+def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and safe_str_cmp(user.password, password): # flask safe way of comparing strings
+    if user and safe_str_cmp(user.password, password):  # flask safe way of comparing strings
         return user
+
 
 def identity(payload):
     user_id = payload['identity']
